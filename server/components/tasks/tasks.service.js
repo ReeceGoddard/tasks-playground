d2a0afb41e4e4e8025037e32e42c443d2cbfb6d3
Reece Goddard <reece.goddard@caseware.com>
2021-04-05 16:50:15 -0400

    add task service with simple functions

---
+++ server/components/tasks/tasks.service.js
@@ -0,0 +1,38 @@
+// import MongooseService from "./mongoose.service"; // Data Access Layer
+import { TaskModel } from "./task.model";
+
+const TasksService = {};
+
+TasksService.getAll = async function () {
+  try {
+    return await TaskModel.find();
+  } catch (err) {
+    return err;
+  }
+};
+
+TasksService.create = async function (task) {
+  try {
+    return TaskModel.create(task);
+  } catch (err) {
+    return err;
+  }
+};
+
+TasksService.patch = async function (id, task) {
+  try {
+    return await TaskModel.findByIdAndUpdate(id, task, { new: true });
+  } catch (err) {
+    return err;
+  }
+};
+
+TasksService.deleteById = async function (id) {
+  try {
+    return TaskModel.findByIdAndDelete(id);
+  } catch (err) {
+    return err;
+  }
+};
+
+export default TasksService;
