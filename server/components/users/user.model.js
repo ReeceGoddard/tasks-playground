e678b14655f85e1c13fa159b1bf9caf737caed9b
Reece Goddard <reece.goddard@caseware.com>
2021-04-07 19:10:51 -0400

    add fields to User model

--- server/components/users/user.model.js
+++ server/components/users/user.model.js
@@ -1,10 +1,31 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { config } from "dotenv";
+import validator from "validator";
+config();
 
-export const userSchema = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
+      required: [true, "Name is required"],
+    },
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      validate: [validator.isEmail, "Please provide a valid email address"],
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 8,
     },
   },
   {
@@ -12,4 +33,6 @@ export const userSchema = new mongoose.Schema(
   }
 );
 
-export const UserModel = mongoose.model("User", userSchema);
+const UserModel = mongoose.model("User", userSchema);
+
+export default UserModel;
