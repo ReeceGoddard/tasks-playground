ad177842ec74cb5710eec7e48a57887cdd9c8c4e
Reece Goddard <reece.goddard@caseware.com>
2021-04-07 19:19:46 -0400

    update www to use mongo helper connector

--- server/bin/www.js
+++ server/bin/www.js
@@ -4,17 +4,20 @@
  * Module dependencies.
  */
 
+import { config } from "dotenv";
+config();
 import { app } from "../app.js";
 import debugLib from "debug";
 import http from "http";
-import mongoose from "mongoose";
-const debug = debugLib("tasks-playground:server");
+import connectToMongo from "../shared/mongo-connect";
+
+const debug = debugLib("dev");
 
 /**
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || "9000");
+const port = normalizePort(process.env.PORT || "4000");
 app.set("port", port);
 
 /**
@@ -85,20 +88,5 @@ function onListening() {
   var addr = server.address();
   var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
   debug("Listening on " + bind);
-  connectToDB();
-}
-
-function connectToDB() {
-  const dev_db_url = "mongodb://localhost:27017/tasks-playground";
-
-  mongoose.connect(process.env.MONGODB_URI || dev_db_url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-
-  const db = mongoose.connection;
-  db.once("open", () => console.log("Connected to the database"));
-  db.on("error", () =>
-    console.error.bind(console, "MongoDB connection error:")
-  );
+  connectToMongo();
 }
